4bb540879ebf2708b9d57dcea6d09582845d8896
sahil <mdhannanit2021@gmail.com>
2025-09-03 15:05:50 +0600

    change localhost url to  vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 
-// Explicitly set the runtime to 'nodejs'
-export const runtime = "nodejs";
-
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
